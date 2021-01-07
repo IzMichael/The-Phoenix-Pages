aed91191935ab77970f4274ba2fc862ad5f9f5c5
IzMichael <themichael77@outlook.com>
2021-01-07 18:48:41 +1300

    Add cycling Tips in header

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+var words = [
+    "You can click on the coordinates to go to those coordinates on Dynmap",
+    "You can add a shop or farm by clicking the 'Add' button below the database"
+];
+var i = 0;
+var text = "This text changes";
+
+function getChangedText() {
+    i = (i + 1) % words.length;
+    return text.replace(/This text changes/, words[i]);
+}
+
+function changeText() {
+    var txt = getChangedText();
+    var d = document.getElementById("tips");
+    d.innerHTML = 'Tip: ' + txt;
+}
+changeText()
+setInterval("changeText()", 10000);
